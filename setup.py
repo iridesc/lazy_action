b901005be1d8fdc12b987b66744ede096041154a
zhangziyuan <zhangziyuan@nsfocus.com>
2025-07-21 17:40:11 +0800

    更新了文档

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lazy_action",
-    version="0.1.1",
+    version="0.1.3",
     author="Irid",
     author_email="irid.zzy@gmail.com",
     description="lazy_action make your func lazy",
